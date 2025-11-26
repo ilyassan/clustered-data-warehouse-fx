97a3bb3db3d3e417a68d48d2e94b4837fa3779e6
ilyass anida <anidailyass@gmail.com>
2025-11-26 10:27:51 +0100

    first commit

---
+++ src/main/java/com/ilyassan/clustereddatawarehousefx/ClusteredDataWarehouseFxApplication.java
@@ -0,0 +1,13 @@
+package com.ilyassan.clustereddatawarehousefx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusteredDataWarehouseFxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClusteredDataWarehouseFxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilyassan/clustereddatawarehousefx/ClusteredDataWarehouseFxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilyassan.clustereddatawarehousefx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusteredDataWarehouseFxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
