cb7721aa6bd2ddd3db5c18cf68e8c44cacfdc8de
ilyass anida <anidailyass@gmail.com>
2025-11-26 15:13:39 +0100

    feat: (CDWF-22) Add comprehensive API documentation using Swagger/OpenAPI

---
+++ src/main/java/com/ilyassan/clustereddatawarehousefx/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.ilyassan.clustereddatawarehousefx.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("FX Deals Data Warehouse API")
+                        .version("1.0.0")
+                        .description("Bloomberg FX Deals Data Warehouse - REST API for importing and managing foreign exchange deal transactions")
+                        .contact(new Contact()
+                                .name("ProgressSoft Corporation")
+                                .email("support@progresssoft.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Development Server")
+                ));
+    }
+}
